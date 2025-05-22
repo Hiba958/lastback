476c198f6b62b79292252dc03b1600900da8e990
Hiba958 <amarhiba010@gmail.com>
2025-05-22 09:51:44 +0100

    Update sensorRoutes.js

--- routes/sensorRoutes.js
+++ routes/sensorRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addSensorData, getLatestSensorData } = require('../controllers/sensorController');
+const { addSensorData, getLatestSensorData, controlDevice } = require('../controllers/sensorController');
 
 router.post('/add', addSensorData);           // Appelé par ESP32
 router.get('/latest', getLatestSensorData);   // Appelé par le frontend
+router.post('/control', controlDevice);       // Appelé par frontend et ESP32
 
 module.exports = router;
